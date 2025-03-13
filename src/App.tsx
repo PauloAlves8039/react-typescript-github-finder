e48d4b35413e5a2d9583ccdd7d662c9f6302ebd1
PauloAlves8039 <pj38alves@gmail.com>
2025-03-12 21:46:57 -0300

    feat: cria configurações das rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 export default function App() {
   return (
     <>
       <div>
         <h1>GitHub Finder</h1>
+        < Outlet />
       </div>
     </>
   );
