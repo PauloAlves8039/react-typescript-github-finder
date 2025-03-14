8f196964a338e40875edef47eabdc41dd727bcd7
PauloAlves8039 <pj38alves@gmail.com>
2025-03-13 21:13:32 -0300

    feat: adiciona o componente search

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import Search from "../components/Search/Search";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <Search />
+    </div>
   );
 }
