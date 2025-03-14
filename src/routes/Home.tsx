40bd8ea6136c6c77983d3637886310eb3954ffc6
PauloAlves8039 <pj38alves@gmail.com>
2025-03-14 20:57:13 -0300

    feat: implementa pesquisa de usuário

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,21 @@
 import Search from "../components/Search/Search";
+import { useState } from "react";
+import { UserProps } from "../types/user";
 
 export default function Home() {
+  const [ user, setUser ] = useState<UserProps | null>(null);
+  const url: string = "https://api.github.com/users/";
+
+  const loadUser = async(userName: string) => {
+    const response = await fetch(`${url}${userName}`);
+    const data = await response.json();
+
+    console.log(data);
+  }
+
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
     </div>
   );
 }
