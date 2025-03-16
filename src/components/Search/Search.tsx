0d26b9c0ec46863b2075edb939f3e241b46442ca
PauloAlves8039 <pj38alves@gmail.com>
2025-03-16 19:52:33 -0300

    feat: adiciona pesquisa ao pressionar enter e limpeza de input

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,5 +1,5 @@
 import { BsSearch } from "react-icons/bs";
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 import classes from "./Search.module.css";
 
 type SearchProps = {
@@ -9,6 +9,17 @@ type SearchProps = {
 export default function Search({loadUser}: SearchProps) {
     const [ userName, setUserName ] = useState("");
     
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === "Enter") {
+            loadUser(userName);
+            clearSearch();
+        }
+    };
+
+    const clearSearch = () => {
+        setUserName("");
+    };
+
     return (
         <div className={classes.search}>
             <h2>Busque por um usuário:</h2>
@@ -16,10 +27,15 @@ export default function Search({loadUser}: SearchProps) {
             <div className={classes.search_container}>
                 <input 
                     type="text" 
-                    placeholder="Digite o nome do usuário" 
-                    onChange={(e) => setUserName(e.target.value)} 
+                    placeholder="Digite o nome do usuário"
+                    value={userName} 
+                    onChange={(e) => setUserName(e.target.value)}
+                    onKeyDown={handleKeyDown} 
                 />
-                <button onClick={() => loadUser(userName)}>
+                <button onClick={() => {
+                    loadUser(userName);
+                    clearSearch();
+                }}>
                     <BsSearch />
                 </button>
             </div>
