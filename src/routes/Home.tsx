b3d287918cd981c343063d09f335a53ee16a6be9
PauloAlves8039 <pj38alves@gmail.com>
2025-03-18 21:19:35 -0300

    feat: aplica componente de erro

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,14 +2,23 @@ import Search from "../components/Search/Search";
 import { useState } from "react";
 import { UserProps } from "../types/user";
 import User from "../components/User/User";
+import Error from "../components/Error/Error";
 
 export default function Home() {
   const [ user, setUser ] = useState<UserProps | null>(null);
+  const [ error, setError ] = useState(false);
   const url: string = "https://api.github.com/users/";
 
   const loadUser = async(userName: string) => {
+    setError(false);
+    setUser(null);
     const response = await fetch(`${url}${userName}`);
     const data = await response.json();
+
+    if (response.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -29,6 +38,7 @@ export default function Home() {
     <div>
       <Search loadUser={loadUser} />
       { user && <User {...user} /> }
+      { error && <Error /> }
     </div>
   );
 }
