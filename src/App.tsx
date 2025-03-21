c298050fb3e42dedd931600affe1e56c143ed44f
PauloAlves8039 <pj38alves@gmail.com>
2025-03-21 20:47:52 -0300

    feat: implementa ícone do github

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
+import { FaGithub } from "react-icons/fa";
 import classes from "./App.module.css";
 
 export default function App() {
   return (
     <>
       <div className={classes.app}>
-        <h1>GitHub Finder</h1>
+        <h1>
+          <FaGithub className={classes.github_icon} /> GitHub Finder
+        </h1>
         < Outlet />
       </div>
     </>
